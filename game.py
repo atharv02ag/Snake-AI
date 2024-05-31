2ecb07034ee5f5abd78467fdd69198a492aea0be
atharv02ag <atharvagarwal2718@gmail.com>
2024-05-31 13:23:05 +0530

    snake movement and food implemented

--- game.py
+++ game.py
@@ -15,11 +15,57 @@ class Game():
         self.snake_sprites = pygame.sprite.Group()
         self.snake = Snake(self.snake_sprites)  
 
-        self.timers = {"MOVE" : Timer(SNAKE_TIME,self.snake.move)}
+        self.timers = {"MOVE" : Timer(SNAKE_TIME,self.snake.move),
+                       "INPUT" : Timer(INPUT_TIME,self.input)}
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if(keys[pygame.K_UP]):
+            self.snake.v_x = 0
+            self.snake.v_y = (-1)*GRID_CELL
+            self.snake.head.dir = (0,-1)
+        elif(keys[pygame.K_DOWN]):
+            self.snake.v_x = 0
+            self.snake.v_y = 1*GRID_CELL
+            self.snake.head.dir = (0,1)
+        elif(keys[pygame.K_RIGHT]):
+            self.snake.v_x = 1*GRID_CELL
+            self.snake.v_y = 0
+            self.snake.head.dir = (1,0)
+        elif(keys[pygame.K_LEFT]):
+            self.snake.v_x = (-1)*GRID_CELL
+            self.snake.v_y = 0
+            self.snake.head.dir = (-1,0)
+
+    def check_boundary(self):
+        if(self.snake.head.x < 0): #left boundary 
+            self.snake.head.x = GAME_WIDTH-GRID_CELL
+
+        elif(self.snake.head.x > GAME_WIDTH): #right boundary
+            self.snake.head.x = 0
+        
+        elif(self.snake.head.y < 0): #top boundary
+            self.snake.head.y = GAME_HEIGHT-GRID_CELL
+        
+        elif(self.snake.head.y > GAME_HEIGHT): #bottom boundary
+            self.snake.head.y = 0
+
+    def respawn_food(self):
+        self.food.sprite = Food(self.food)
+
+    def check_food(self):
+        if(pygame.sprite.groupcollide(self.food,self.snake_sprites,False,False)):
+            self.respawn_food()
+            self.snake.append_part(self.snake_sprites)
+
 
     def run(self):
 
         self.timers["MOVE"].runTimer()
+        self.timers["INPUT"].runTimer()
+        self.check_boundary()
+        self.check_food()
         
         self.screen.blit(self.game_screen, self.game_rect)
         self.game_screen.fill(GAME_COLOR) #VERY IMPORTANT : TO FILL OVER (CLEAR) CONTENTS OF PREVIOUS FRAME
--- snake.py
+++ snake.py
@@ -2,14 +2,15 @@ from settings import *
 
 class Part(pygame.sprite.Sprite):
 
-    def __init__(self,group,color):
+    def __init__(self,group,color,x,y,dir):
         super().__init__(group)
         self.image = pygame.surface.Surface((GRID_CELL,GRID_CELL))
         self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
         self.rect.topleft = (self.x,self.y)
+        self.dir = dir #direction of motion - (1,0), (-1,0), (0,1), (0,-1)
 
     def update(self):
         self.rect.topleft = (self.x,self.y)
@@ -17,12 +18,30 @@ class Part(pygame.sprite.Sprite):
 class Snake(pygame.sprite.Sprite):
 
     def __init__(self,group):
-        self.head = Part(group,SNAKE_HEAD_COLOR)
+        self.head = Part(group,SNAKE_HEAD_COLOR,0,0,'r')
         self.parts = [self.head]
         self.v_x = 1*GRID_CELL
         self.v_y = 0
 
     def move(self):
+        prev_pos = (self.head.x,self.head.y)
+        prev_dir = self.head.dir
+    
         for part in self.parts:
-            part.x += self.v_x
-            part.y += self.v_y
+            if(part == self.head):
+                part.x += self.v_x
+                part.y += self.v_y
+            else:
+                temp_pos = (part.x,part.y)
+                (part.x,part.y) = prev_pos
+                prev_pos = temp_pos
+
+                temp_dir = part.dir
+                part.dir = prev_dir
+                prev_dir = temp_dir
+    
+    def append_part(self, group):
+        last_part = self.parts[len(self.parts)-1]
+        pos = (last_part.x-last_part.dir[0]*GRID_CELL, last_part.y-last_part.dir[1]*GRID_CELL)
+        new_part = Part(group,SNAKE_BODY_COLOR,pos[0],pos[1],last_part.dir)
+        self.parts.append(new_part)
