5702607e3f6655c1db4e3d0a471db00830fc873a
atharv02ag <atharvagarwal2718@gmail.com>
2024-06-03 15:42:30 +0530

    neat implemented but not good

--- game.py
+++ game.py
@@ -3,6 +3,9 @@ from food import Food
 from snake import Snake
 from timer import Timer
 
+import neat
+import neat.config
+
 class Game():
 
     def __init__(self):
@@ -12,72 +15,108 @@ class Game():
 
         self.food = pygame.sprite.GroupSingle()
         food = Food(self.food)
-        self.snake_sprites = pygame.sprite.Group()
-        self.snake = Snake(self.snake_sprites)  
 
-        self.timers = {"MOVE" : Timer(SNAKE_TIME,self.snake.move),
-                       "INPUT" : Timer(INPUT_TIME,self.input)}
+        self.snakes = []
+        self.snake_sprites_all = []
+        self.neural_networks = []
+        self.timers_all = []
+        self.ge = []
+
+    def init_neat(self,genomes,config):
+        for x,g in genomes:
+            net = neat.nn.FeedForwardNetwork.create(g,config)
+            self.neural_networks.append(net)
+
+            snake_sprites = pygame.sprite.Group()
+            self.snake_sprites_all.append(snake_sprites)
+
+            snake = Snake(snake_sprites)  
+            self.snakes.append(snake)
+
+            timers = {"MOVE" : Timer(SNAKE_TIME,snake.move),
+                            "INPUT" : Timer(INPUT_TIME,self.input)}
+            self.timers_all.append(timers)
+
+            g.fitness = 0
+            self.ge.append(g)
+
     
     def input(self):
-        keys = pygame.key.get_pressed()
+        
+        for i,snake in enumerate(self.snakes):
+            output = self.neural_networks[i].activate((self.snakes[i].head.x, self.snakes[i].head.y, self.food.sprite.x, self.food.sprite.y))
 
-        if(keys[pygame.K_UP]):
-            self.snake.v_x = 0
-            self.snake.v_y = (-1)*GRID_CELL
-            self.snake.head.dir = (0,-1)
-        elif(keys[pygame.K_DOWN]):
-            self.snake.v_x = 0
-            self.snake.v_y = 1*GRID_CELL
-            self.snake.head.dir = (0,1)
-        elif(keys[pygame.K_RIGHT]):
-            self.snake.v_x = 1*GRID_CELL
-            self.snake.v_y = 0
-            self.snake.head.dir = (1,0)
-        elif(keys[pygame.K_LEFT]):
-            self.snake.v_x = (-1)*GRID_CELL
-            self.snake.v_y = 0
-            self.snake.head.dir = (-1,0)
+            if(output[0]>0.5):
+                self.snakes[i].v_x = 0
+                self.snakes[i].v_y = (-1)*GRID_CELL
+                self.snakes[i].head.dir = (0,-1)
+                self.ge[i].fitness += 0.1
+            elif(output[1]>0.5):
+                self.snakes[i].v_x = 0
+                self.snakes[i].v_y = 1*GRID_CELL
+                self.snakes[i].head.dir = (0,1)
+                self.ge[i].fitness += 0.1
+            elif(output[2]>0.5):
+                self.snakes[i].v_x = 1*GRID_CELL
+                self.snakes[i].v_y = 0
+                self.snakes[i].head.dir = (1,0)
+                self.ge[i].fitness += 0.1
+            elif(output[3]>0.5):
+                self.snakes[i].v_x = (-1)*GRID_CELL
+                self.snakes[i].v_y = 0
+                self.snakes[i].head.dir = (-1,0)
+                self.ge[i].fitness += 0.1
 
     def check_boundary(self):
-        if(self.snake.head.x < 0): #left boundary 
-            self.snake.head.x = GAME_WIDTH-GRID_CELL
+        for snake in self.snakes:
+            if(snake.head.x < 0): #left boundary 
+                snake.head.x = GAME_WIDTH-GRID_CELL
 
-        elif(self.snake.head.x > GAME_WIDTH): #right boundary
-            self.snake.head.x = 0
-        
-        elif(self.snake.head.y < 0): #top boundary
-            self.snake.head.y = GAME_HEIGHT-GRID_CELL
-        
-        elif(self.snake.head.y > GAME_HEIGHT): #bottom boundary
-            self.snake.head.y = 0
+            elif(snake.head.x > GAME_WIDTH): #right boundary
+                snake.head.x = 0
+            
+            elif(snake.head.y < 0): #top boundary
+                snake.head.y = GAME_HEIGHT-GRID_CELL
+            
+            elif(snake.head.y > GAME_HEIGHT): #bottom boundary
+                snake.head.y = 0
 
     def respawn_food(self):
         self.food.sprite = Food(self.food)
 
     def check_food(self):
-        if(pygame.sprite.groupcollide(self.food,self.snake_sprites,False,False)):
-            self.respawn_food()
-            self.snake.append_part(self.snake_sprites)
-            set_score(len(self.snake.parts)-1)
+        for i,snake_sprites in enumerate(self.snake_sprites_all):
+
+            if(pygame.sprite.groupcollide(self.food,self.snake_sprites_all[i],False,False)):
+                self.respawn_food()
+                self.snakes[i].append_part(self.snake_sprites_all[i])
+                self.ge[i].fitness += 10
+                #set_score(len(self.snakes[i].parts)-1)
 
     def check_self_collision(self):
-        for part in self.snake.parts:
-            if(part != self.snake.head and pygame.sprite.collide_rect(part,self.snake.head)):
-                self.reset()
-                break
+        for i,snake in enumerate(self.snakes):
+            for part in self.snakes[i].parts:
+                if(part != self.snakes[i].head and pygame.sprite.collide_rect(part,self.snakes[i].head)):
+                    self.ge[i].fitness -= 2
+                    self.snakes.pop(i)
+                    self.neural_networks.pop(i)
+                    self.ge.pop(i)
+                    break
 
-    def reset(self):
-        set_score(0)
-        self.respawn_food()
-        self.snake_sprites.empty()
-        self.snake = Snake(self.snake_sprites)
-        self.timers = {"MOVE" : Timer(SNAKE_TIME,self.snake.move),
-                       "INPUT" : Timer(INPUT_TIME,self.input)}
+    # def reset(self):
+    #     set_score(0)
+    #     self.respawn_food()
+    #     self.snake_sprites.empty()
+    #     self.snake = Snake(self.snake_sprites)
+    #     self.timers = {"MOVE" : Timer(SNAKE_TIME,self.snake.move),
+    #                    "INPUT" : Timer(INPUT_TIME,self.input)}
 
     def run(self):
 
-        self.timers["MOVE"].runTimer()
-        self.timers["INPUT"].runTimer()
+        for timers in self.timers_all:
+            timers["MOVE"].runTimer()
+            timers["INPUT"].runTimer()
+
         self.check_boundary()
         self.check_food()
         self.check_self_collision()
@@ -85,7 +124,8 @@ class Game():
         self.screen.blit(self.game_screen, self.game_rect)
         self.game_screen.fill(GAME_COLOR) #VERY IMPORTANT : TO FILL OVER (CLEAR) CONTENTS OF PREVIOUS FRAME
    
-        self.snake_sprites.update()
+        for snake_sprites in self.snake_sprites_all:
+            snake_sprites.update()
+            snake_sprites.draw(self.game_screen)
 
-        self.snake_sprites.draw(self.game_screen)
         self.food.draw(self.game_screen)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from sys import exit
+
 from settings import *
 from game import Game
 from panel import Panel
+
+import os
+import neat
+import neat.config
+
 
 class Main():
     def __init__(self):
@@ -11,8 +17,9 @@ class Main():
         self.game = Game()
         self.panel = Panel()
 
-    def run(self):
+    def eval_genomes(self,genomes,config):
         running = True
+        self.game.init_neat(genomes,config)
         while(running):
             for event in pygame.event.get():
                 if(event.type == pygame.QUIT):
@@ -27,7 +34,23 @@ class Main():
 
             pygame.display.update()
             self.clock.tick(FPS)
+    
+    def run(self,config_path):
+        config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+                                    neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+        pop = neat.Population(config)
+        pop.add_reporter(neat.StdOutReporter(True))
+
+        stats = neat.StatisticsReporter()
+        pop.add_reporter(stats)
+
+        winner = pop.run(self.eval_genomes,50)
 
 if(__name__ == '__main__'):
+
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir,"neat-config.txt")
+
     main = Main()
-    main.run()
+    main.run(config_path)
+
